0eaed529fd2a71b8237ccd634ffb2c0584a82ef6
Yongjie Liu <liuyongjie@rd.netease.com>
2020-05-12 11:01:35 +0800

    Update train_netease_noise.py

--- train_netease_noise.py
+++ train_netease_noise.py
@@ -186,7 +186,7 @@ for eidx in range(max_epochs):
         tap_a_mask = torch.from_numpy(tap_a_mask).cuda()
 
         # 一,tparams_p_u,tparams_p_ls2只因被初始化一次，但prior_u,prior_s2,Beta需要被重新计算
-        prior_u, prior_s2 = weight_noise_class.f_apply_noise_to_weight(TAP_model)
+        prior_u, prior_s2 = weight_noise.f_apply_noise_to_weight(TAP_model)
         # forward
         tap_ctx, cost_alphas = TAP_model(params, tap_x, tap_x_mask, tap_a, tap_a_mask, tap_y,
                                          tap_y_mask)
